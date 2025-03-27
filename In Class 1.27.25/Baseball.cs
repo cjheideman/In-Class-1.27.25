4340559668f0001ef4537643bd964452ba4f3c09
Caleb Heideman <cjheideman2003@gmail.com>
2025-03-27 12:14:57 -0400

    Add project files.

---
+++ In Class 1.27.25/Baseball.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_1._27._25
+{
+    internal class Baseball
+    {
+        private double _price; 
+
+        public double Price { get { return _price; } set { _price = value; } }
+    }
+}
---
+++ In Class 1.27.25/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_1._27._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Baseball myBaseball = new Baseball();
+            myBaseball.Price = 153.85; 
+            Console.WriteLine("Price of a Red's Ticket is " + myBaseball.Price);
+        }
+    }
+}
